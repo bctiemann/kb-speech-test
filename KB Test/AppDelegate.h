2a80bca84ee3e9b8c5515266682ecbd6c53ad887
Brian Tiemann <brian.tiemann@essencedigital.com>
2015-12-28 11:51:38 -0500

    Initial Commit

---
+++ KB Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KB Test
+//
+//  Created by brian.tiemann on 12/28/15.
+//  Copyright © 2015 brian.tiemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KB Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KB Test
+//
+//  Created by brian.tiemann on 12/28/15.
+//  Copyright © 2015 brian.tiemann. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
